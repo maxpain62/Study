939d93816bb7d245576e6a3bba40293c1600304d
maxpain62 <maxpain62@gmail.com>
2022-09-19 08:47:20 +0530

    initial upload

---
+++ shell-scripts/pre-boot-backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+printf "\nprint IP address: \n"
+/usr/sbin/ip r l
+
+printf "\nprint route: \n"
+/usr/sbin/route n
+
+printf "\nprint connected ethernet devices: \n"
+/usr/sbin/ip addr
+
+printf "\nprint connected disks: \n"
+/usr/sbin/fdisk -l
+
+printf "\nprint mounted fs: \n"
+/bin/df -h
+
+printf "\nprint fstab file: \n"
+cat /etc/fstab
+
+printf "\nprint physical volumes: \n"
+/usr/sbin/pvs
+
+printf "\nprint volume groups: \n"
+/usr/sbin/vgs
+
+printf "\nprint logical volumes: \n"
+/usr/sbin/lvs
+
+printf "\nprint kernel: \n"
+/bin/rpm -qa | grep kernel
