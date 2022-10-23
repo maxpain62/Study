1deec5bfc8b19fa3f072b0777e93211a098b3cfd
maxpain62 <112951389+maxpain62@users.noreply.github.com>
2022-10-23 11:13:58 +0530

    create multiple vpc and subnets with .tfvars file

--- QT/terraform/multiple-vpc-subnets/dev.tfvars
+++ QT/terraform/multiple-vpc-subnets/dev.tfvars
@@ -1,10 +1,10 @@
-target_region = "ap-south-1"
+target_region  = "ap-south-1"
 vpc_cidr_block = ["192.168.0.0/16", "10.168.0.0/16"]
 
 primary_public_subnet_cidr_block = ["192.168.0.0/24"]
 primary_public_availability_zone = ["ap-south-1a"]
 
-primary_subnet_cidr_block = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+primary_subnet_cidr_block   = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
 secondary_subnet_cidr_block = ["10.168.0.0/24", "10.168.1.0/24", "10.168.2.0/24", "10.168.3.0/24"]
 
 target_availability_zone = ["ap-south-1a", "ap-south-1b", "ap-south-1c", "ap-south-1a"]
--- QT/terraform/multiple-vpc-subnets/variable.tf
+++ QT/terraform/multiple-vpc-subnets/variable.tf
@@ -1,28 +1,28 @@
 variable "target_region" {
-    type = string  
+  type = string
 }
 
 variable "vpc_cidr_block" {
-    type = list(string)
+  type = list(string)
 }
 
 variable "primary_subnet_cidr_block" {
-    type = list(string)
+  type = list(string)
 }
 
 variable "secondary_subnet_cidr_block" {
-    type = list(string)
-  
+  type = list(string)
+
 }
 
 variable "target_availability_zone" {
-    type = list(string)
+  type = list(string)
 }
 
 variable "primary_public_subnet_cidr_block" {
-    type = list(string)  
+  type = list(string)
 }
 
 variable "primary_public_availability_zone" {
-    type = list(string)  
+  type = list(string)
 }
