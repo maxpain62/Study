ba967b733ee0bc971629607ad2ecd3d8b2a21d4d
maxpain62 <112951389+maxpain62@users.noreply.github.com>
2022-10-23 11:52:25 +0530

    added route table, inet gw, and route table association

--- QT/terraform/multiple-vpc-subnets/vpc.tf
+++ QT/terraform/multiple-vpc-subnets/vpc.tf
@@ -1,9 +1,25 @@
 resource "aws_vpc" "tf_vpc" {
-  count      = 2
+  count      = length(var.vpc_cidr_block)
   cidr_block = var.vpc_cidr_block[count.index]
   tags = {
     "Name" = "tf_vpc_${count.index}"
   }
+}
+
+resource "aws_internet_gateway" "tf_itgw" {
+    vpc_id = aws_vpc.tf_vpc[0].id  
+}
+
+resource "aws_route_table" "tf_rt" {
+    vpc_id = aws_vpc.tf_vpc[0].id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.tf_itgw.id
+    }
+
+    tags = {
+      "Name" = "tf_rt_itgw"
+    }
 }
 
 resource "aws_subnet" "primary_subnet" {
@@ -21,10 +37,16 @@ resource "aws_subnet" "primary_public_subnet" {
   count             = length(var.primary_public_subnet_cidr_block)
   cidr_block        = var.primary_public_subnet_cidr_block[count.index]
   availability_zone = var.primary_public_availability_zone[count.index]
+  map_public_ip_on_launch = true
+  enable_resource_name_dns_a_record_on_launch = true
   tags = {
     "Name" = "primary_public_subnet_${count.index}"
   }
+}
 
+resource "aws_route_table_association" "tf_rta" {
+    subnet_id = aws_subnet.primary_public_subnet[0].id
+    route_table_id = aws_route_table.tf_rt.id
 }
 
 resource "aws_subnet" "secondary_subnet" {
