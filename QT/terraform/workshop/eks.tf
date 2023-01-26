189fb9a4164bf66af6c1e2f01b55b2d80c10d28a
maxpain62 <112951389+maxpain62@users.noreply.github.com>
2023-01-26 18:59:21 +0530

    correction in vpc_config

--- QT/terraform/workshop/eks.tf
+++ QT/terraform/workshop/eks.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "my_cluster" {
     name = "my_cluster"
     role_arn = "arn:aws:iam::515138065344:role/cluster-admin-role"
-    vpc_config = {
+    vpc_config {
         subnet_ids = ["subnet-0fb6aa3631bea2cfd", "subnet-0aec41c32cf6a3788"]
         }
   
